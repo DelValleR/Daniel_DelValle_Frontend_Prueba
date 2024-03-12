b7b1dbd58778d445a9da6da68f462cdf5269984a
Daniel Del Valle R <97266451+DelValleR@users.noreply.github.com>
2024-03-12 06:09:41 -0500

    RadioInput component

---
+++ src/components/RadioInput/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './RadioInput.scss';
+
+const RadioInput = ({ idPrefix, options, selectedOption, onChange }) => {
+	return (
+		<div className="radio-input">
+			{options.map((option, index) => (
+				<div key={index} className="radio-item">
+					<input
+						type="radio"
+						id={`${idPrefix}-${index}`}
+						name={idPrefix}
+						value={option}
+						checked={selectedOption === option}
+						onChange={() => onChange(option)}
+					/>
+					<label htmlFor={`${idPrefix}-${index}`}>{option}</label>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default RadioInput;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,25 @@
 import TopWrapper from '../../components/TopWrapper';
 import Form from '../../components/Form';
 import styles from './Home.scss';
+import RadioInput from '../../components/RadioInput';
+import { useState } from 'react';
 
 const Home = () => {
+	const [selectedOption, setSelectedOption] = useState('');
+
+	const handleOptionChange = (option) => {
+		setSelectedOption(option);
+	};
 	return (
 		<div className={styles.App}>
 			<TopWrapper />
 			<Form />
+			<RadioInput
+				idPrefix="options"
+				options={['Opción 1', 'Opción 2', 'Opción 3', 'Opción 4']}
+				selectedOption={selectedOption}
+				onChange={handleOptionChange}
+			/>
 		</div>
 	);
 };
